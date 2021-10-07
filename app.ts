2f4a1aa0d760b90c650fa38c70a481ddcacd70e2
Colby Adams <colby.adams@heapanalytics.com>
2021-10-07 11:03:55 -0500

    added prettier, fixed versions

--- app.ts
+++ app.ts
@@ -1,42 +1,39 @@
 import Router = require('koa-router');
-import logger = require('koa-logger')
+import logger = require('koa-logger');
 import Koa = require('koa');
-import {usersSync} from "./webhooks/usersSync";
-import {usersDrain} from "./webhooks/usersDrain";
-import {validateHeapHeader} from "./middleware/validateHeapHeader";
-import {Context} from "koa";
+import { usersSync } from './webhooks/usersSync';
+import { usersDrain } from './webhooks/usersDrain';
+import { validateHeapHeader } from './middleware/validateHeapHeader';
+import { Context } from 'koa';
 
-const process = require('process')
+const process = require('process');
 const bodyParser = require('koa-bodyparser');
-
 
 const app = new Koa();
 const router = new Router();
 
-const healthCheck =  async (ctx: Context, next: () => Promise<void>): Promise<void> => {
-    if (ctx.path === "/healthcheck") {
-        ctx.status = 200;
-    } else {
-        await next();
-    }
+const healthCheck = async (ctx: Context, next: () => Promise<void>): Promise<void> => {
+  if (ctx.path === '/healthcheck') {
+    ctx.status = 200;
+  } else {
+    await next();
+  }
 };
 
-router
-    .post('/users_sync', usersSync)
-    .post('/users_drain', usersDrain)
+router.post('/users_sync', usersSync).post('/users_drain', usersDrain);
 
-app.use(healthCheck)
+app.use(healthCheck);
 app.use(logger());
 app.use(bodyParser());
 app.use(validateHeapHeader);
 app.use(router.routes());
 app.on('error', (err) => {
-    console.log(err)
+  console.log(err);
 });
 
 process.on('SIGINT', () => {
-    console.info("Interrupted")
-    process.exit(0)
-})
+  console.info('Interrupted');
+  process.exit(0);
+});
 
 app.listen(3000);
--- webhooks/usersSync.ts
+++ webhooks/usersSync.ts
@@ -1,53 +1,53 @@
-import { Context } from "koa";
-import { SyncRequest } from "./webhooks_types";
-import { jsonToSyncRequest, getActionTypeFromContext } from "./bodyHelpers";
-import { SEGMENT_USERS_SYNC } from "../constants";
+import { Context } from 'koa';
+import { SyncRequest } from './webhooks_types';
+import { jsonToSyncRequest, getActionTypeFromContext } from './bodyHelpers';
+import { SEGMENT_USERS_SYNC } from '../constants';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const usersSync = async (ctx: Context, next: () => Promise<any>): Promise<any> => {
-    // Validate that this is a request for this endpoint to handle
-    const actionType:string = getActionTypeFromContext(ctx);
-    if (actionType != SEGMENT_USERS_SYNC) {
-        ctx.throw(405, `This endpoint does not support specified actionType: ${actionType}`);
-    }
+  // Validate that this is a request for this endpoint to handle
+  const actionType: string = getActionTypeFromContext(ctx);
+  if (actionType != SEGMENT_USERS_SYNC) {
+    ctx.throw(405, `This endpoint does not support specified actionType: ${actionType}`);
+  }
 
-    let userRequestData: SyncRequest;
-    try {
-        userRequestData = jsonToSyncRequest(ctx.request.body);
-    } catch (e) {
-        ctx.throw(400, `Invalid data format: ${e}`)
-    }
+  let userRequestData: SyncRequest;
+  try {
+    userRequestData = jsonToSyncRequest(ctx.request.body);
+  } catch (e) {
+    ctx.throw(400, `Invalid data format: ${e}`);
+  }
 
-    // Partner Actions:
-    // 1 - Validate the id_token which identifies the customer.  If no id_token is provided
-    //     the account / environment the one in which the partner app is registered. Ex:
-    //         let id_token; // Internal identifier for the customer
-    //         if (!userRequestData.id_token) {
-    //             id_token = SELF_ID_TOKEN
-    //         } else {
-    //             // getInternalIdToken will lookup the id_token passed in and return the
-    //             // internal representation of the customer for the partner
-    //             id_token = getInternalIdToken(userRequestData.id_token)
-    //             if (!id_token) {
-    //                 ctx.throw(400, "Invalid id_token");
-    //             }
-    //         }
-    // 2 - Validate the segment ID.  If it does not exist yet, it should be created.
+  // Partner Actions:
+  // 1 - Validate the id_token which identifies the customer.  If no id_token is provided
+  //     the account / environment the one in which the partner app is registered. Ex:
+  //         let id_token; // Internal identifier for the customer
+  //         if (!userRequestData.id_token) {
+  //             id_token = SELF_ID_TOKEN
+  //         } else {
+  //             // getInternalIdToken will lookup the id_token passed in and return the
+  //             // internal representation of the customer for the partner
+  //             id_token = getInternalIdToken(userRequestData.id_token)
+  //             if (!id_token) {
+  //                 ctx.throw(400, "Invalid id_token");
+  //             }
+  //         }
+  // 2 - Validate the segment ID.  If it does not exist yet, it should be created.
 
-    console.log(
-        `Syncing page ${userRequestData.data.sync_info.page_number} of ${userRequestData.data.sync_info.total_pages}
-        for sync task ${userRequestData.data.sync_info.sync_task_id}`
-    );
+  console.log(
+    `Syncing page ${userRequestData.data.sync_info.page_number} of ${userRequestData.data.sync_info.total_pages}
+        for sync task ${userRequestData.data.sync_info.sync_task_id}`,
+  );
 
-    for (const email of userRequestData.data.add) {
-        // process the new emails
-        console.log(`Adding email ${email.id} for segment ${userRequestData.data.segment.id}`);
-    }
+  for (const email of userRequestData.data.add) {
+    // process the new emails
+    console.log(`Adding email ${email.id} for segment ${userRequestData.data.segment.id}`);
+  }
 
-    for (const email of userRequestData.data.remove) {
-        // process the emails that should be removed
-        console.log(`Removing email ${email.id} for segment ${userRequestData.data.segment.id}`);
-    }
-    ctx.status = 200;
-    await next();
-}
+  for (const email of userRequestData.data.remove) {
+    // process the emails that should be removed
+    console.log(`Removing email ${email.id} for segment ${userRequestData.data.segment.id}`);
+  }
+  ctx.status = 200;
+  await next();
+};
