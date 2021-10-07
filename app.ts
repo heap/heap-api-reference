2cba64693cb74bff61a2319e255cf0c1651fc80d
Colby Adams <colby.adams@heapanalytics.com>
2021-10-07 10:49:49 -0500

    updated app.ts

--- app.ts
+++ app.ts
@@ -1,19 +1,31 @@
 import Router = require('koa-router');
 import logger = require('koa-logger')
 import Koa = require('koa');
-import { usersSync } from "./webhooks/usersSync";
-import { usersDrain } from "./webhooks/usersDrain";
-import { validateHeapHeader } from "./middleware/validateHeapHeader";
+import {usersSync} from "./webhooks/usersSync";
+import {usersDrain} from "./webhooks/usersDrain";
+import {validateHeapHeader} from "./middleware/validateHeapHeader";
+import {Context} from "koa";
+
+const process = require('process')
 const bodyParser = require('koa-bodyparser');
 
 
 const app = new Koa();
 const router = new Router();
 
+const healthCheck =  async (ctx: Context, next: () => Promise<void>): Promise<void> => {
+    if (ctx.path === "/healthcheck") {
+        ctx.status = 200;
+    } else {
+        await next();
+    }
+};
+
 router
     .post('/users_sync', usersSync)
     .post('/users_drain', usersDrain)
 
+app.use(healthCheck)
 app.use(logger());
 app.use(bodyParser());
 app.use(validateHeapHeader);
@@ -21,4 +33,10 @@ app.use(router.routes());
 app.on('error', (err) => {
     console.log(err)
 });
+
+process.on('SIGINT', () => {
+    console.info("Interrupted")
+    process.exit(0)
+})
+
 app.listen(3000);
